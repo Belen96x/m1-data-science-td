af29b069f99a25e39d50e918614ca82132852f09
Belen <belen.saavedra.39@gmail.com>
2024-03-18 13:01:25 +0100

    Scrapping exercises using Scrappy

--- class02-scraping/Exercise_sheet_3_scrapy.py
+++ class02-scraping/Exercise_sheet_3_scrapy.py
@@ -1,23 +1,24 @@
 import scrapy
 import json
 
-class WikipediaSpider: #Something is missing here. What exactly?
+class WikipediaSpider(scrapy.Spider): #Something is missing here. What exactly?
     name = "wikipedia"
 
     start_urls = ["https://en.wikipedia.org/wiki/List_of_French_artists"]
 
     def parse(self, response):
-        list_els = #get all links which are in lists on the page
-        list_see_also = #get the links in see also (they are not about the disctinct authors)
+        list_els = response.css('ul li a::attr(href)').getall() 
+        # Find all the elements <a> inside <li> inside <ul> and gave the values with the attribute <href>
+        list_see_also = response.css('span#See_also ~ ul li a::attr(href)').getall()
         res_list = list(set(list_els) - set(list_see_also))
         for link in res_list:
             #check that the link actually exists and is not red
                 yield response.follow(link, callback=self.parse_artist)
         
     def parse_artist(self, response):
-        url = #get url of the page
-        name = # get name of the artist
-        paragraph = # get the first paragraph
+        url = response.url
+        name = response.css('h1#firstHeading > span.mw-page-title-main::text').get()
+        paragraph = ''.join(response.css('p *::text').getall())
         yield {'url': url,
                'name': name,
                'paragraph': paragraph}
